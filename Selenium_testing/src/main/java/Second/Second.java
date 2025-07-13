eb1ab0dd4481ee2fc5bac1d7c7236c47ea60b7a5
syedalihussain2112 <syedalihussain7093@gmail.com>
2025-07-13 22:20:44 +0530

    first_commit

---
+++ Selenium_testing/src/main/java/Second/Second.java
@@ -0,0 +1,10 @@
+package Second;
+
+public class Second {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Selenium_testing/src/test/java/First_Project/Laptop.java
@@ -0,0 +1,10 @@
+package First_Project;
+
+public class Laptop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
